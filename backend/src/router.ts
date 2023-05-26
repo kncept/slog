4d76b521c5803d4598cdd9323e3aaadb773c3fcb
Nick Krul <nicholas.krul@gmail.com>
2023-05-26 14:25:30 +0000

    type fix

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -159,20 +159,24 @@ export default class Router {
             const availableProviders: Array<LoginProvider> = []
 
             logonProviders.forEach(p => {
-                const urlParams = new URLSearchParams({
-                    client_id: p.clientId,
-                    redirect_uri: `${frontendUrl}callback/${p.name}`,
-                    scope: p.claims,
-                    state: 'none',
-
-                })
-                const authorizeUrl = `${p.authorizeUrl}?${urlParams.toString()}`
-
                 if (p.type === 'oauth2') {
-                    availableProviders.push({
-                        name: p.name,
-                        authorizeUrl,
-                    } as LoginProvider)
+                    const urlParams = new URLSearchParams({
+                        client_id: p.clientId,
+                        redirect_uri: `${frontendUrl}callback/${p.name}`,
+                        scope: p.claims,
+                        state: 'none',
+
+                    })
+                    const authorizeUrl = `${p.authorizeUrl}?${urlParams.toString()}`
+
+                    if (p.type === 'oauth2') {
+                        availableProviders.push({
+                            name: p.name,
+                            authorizeUrl,
+                        } as LoginProvider)
+                    }
+                } else if (p.type === 'oidc') {
+                    throw new Error('OIDC with autoconfigure not yet supported')
                 }
             })
             return quickResponse(stringify(availableProviders))
@@ -184,72 +188,79 @@ export default class Router {
             for(let i = 0; i < logonProviders.length; i++) {
                 const p = logonProviders[i]
                 if (p.name === providerId) {
+                    if (p.type === 'oauth2') {
+                        const params = parse(requestBody!.toString()) as Record<string, string>
+                        const urlParams = new URLSearchParams({
+                            client_id: p.clientId,
+                            client_secret: p.clientSecret,
+                            redirect_uri: `${frontendUrl}callback/${p.name}`,
+                            code: params.code,
+                        })
 
-                    const params = parse(requestBody!.toString()) as Record<string, string>
-                    const urlParams = new URLSearchParams({
-                        client_id: p.clientId,
-                        client_secret: p.clientSecret,
-                        redirect_uri: `${frontendUrl}callback/${p.name}`,
-                        code: params.code,
-                    })
-
-                    const oauthToken = await fetch(p.accessTokenUrl, {
-                        method: 'POST',
-                        headers: {
-                            'Accept': 'application/json',
-                        },
-                        body: urlParams,
-                    }).then((res: any) => res.json())
-
-                    if (!oauthToken.error && oauthToken.access_token) {
-
-                        // TODO: make this generic... or dictionary ify this (and params) for streamlined definitions
-                        const userDetails = await fetch(p.userDetailsUrl, {
-                            method: 'GET',
+                        const oauthToken = await fetch(p.accessTokenUrl, {
+                            method: 'POST',
                             headers: {
-                                'Accept': '*/*', // 'application/vnd.github+json',
-                                'Authorization': 'Bearer ' + oauthToken.access_token,
-
-                            }
-                        }).then((res: any) => res.json())
-                        console.log('userDetails', userDetails)
-
-                        let admin = false
-                        if (adminUser.includes("@")) {
-                            admin = adminUser === `${providerId}:${userDetails.email}`
-                        } else {
-                            admin = adminUser === `${providerId}:${userDetails.login}`
-                        }
-
-                        // jwt.verify(authToken, 'private-key')
-                        // const iat = Date.now() // for iat
-
-                        // TODO: move to a Signed JWT with claims for 'email' and 'name' and 'admin'
-                        const authenticatedUser: JwtAuthClaimsToSend = {
-                            // authToken,
-                            email: userDetails.email,
-                            name: userDetails.name || userDetails.login,
-                            admin,
-                            tok: oauthToken.access_token
-                        }
-
-                        const authToken = jwt.sign(
-                            authenticatedUser,
-                            (await keyPair).privateKey,
-                            {
-                                algorithm: 'RS512',
-                                issuer: 'super-simple-blog',
-                                expiresIn: '1d',
-                                subject:`${providerId}:${userDetails.id}`,
-                            })
-                        return {
-                            statusCode: 200,
-                            headers: {
-                                'Content-Type': 'application/jwt',
+                                'Accept': 'application/json',
                             },
-                            body: authToken,
-                        }
+                            body: urlParams,
+                        }).then((res: any) => res.json())
 
+                        if (!oauthToken.error && oauthToken.access_token) {
+
+                            // TODO: make this generic... or dictionary ify this (and params) for streamlined definitions
+                            const userDetails = await fetch(p.userDetailsUrl, {
+                                method: 'GET',
+                                headers: {
+                                    'Accept': '*/*', // 'application/vnd.github+json',
+                                    'Authorization': 'Bearer ' + oauthToken.access_token,
+
+                                }
+                            }).then((res: any) => res.json())
+                            console.log('userDetails', userDetails)
+
+                            let admin = false
+
+                            // assumed common in 'user details endpoint' responses:
+                            // email name login id
+
+                            if (adminUser.includes("@")) {
+                                admin = adminUser === `${providerId}:${userDetails.email}`
+                            } else {
+                                admin = adminUser === `${providerId}:${userDetails.login}`
+                            }
+
+                            // jwt.verify(authToken, 'private-key')
+                            // const iat = Date.now() // for iat
+
+                            // TODO: move to a Signed JWT with claims for 'email' and 'name' and 'admin'
+                            const authenticatedUser: JwtAuthClaimsToSend = {
+                                // authToken,
+                                email: userDetails.email,
+                                name: userDetails.name || userDetails.login,
+                                admin,
+                                tok: oauthToken.access_token
+                            }
+
+                            const authToken = jwt.sign(
+                                authenticatedUser,
+                                (await keyPair).privateKey,
+                                {
+                                    algorithm: 'RS512',
+                                    issuer: 'super-simple-blog',
+                                    expiresIn: '1d',
+                                    subject:`${providerId}:${userDetails.id}`,
+                                })
+                            return {
+                                statusCode: 200,
+                                headers: {
+                                    'Content-Type': 'application/jwt',
+                                },
+                                body: authToken,
+                            }
+
+                        }
+                    } else if (p.type === 'oidc') {
+                        throw new Error('OIDC with autoconfigure not yet supported')
                     }
                 }
             }
